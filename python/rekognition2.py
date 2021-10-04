f1e16154d7b5feae3cc32c04b5a7054ac6fa2621
ykoon <ykoon@me.com>
2021-10-04 19:37:55 +0900

    rekoginition code

---
+++ python/rekognition2.py
@@ -0,0 +1,25 @@
+import boto3
+
+def detect_labels_local_file(photo):
+
+
+    client=boto3.client('rekognition')
+   
+    with open(photo, 'rb') as image:
+        response = client.detect_labels(Image={'Bytes': image.read()})
+        
+    print('Detected labels in ' + photo)    
+    for label in response['Labels']:
+        print (label['Name'] + ' : ' + str(label['Confidence']))
+
+    return len(response['Labels'])
+
+def main():
+    photo='photo'
+
+    label_count=detect_labels_local_file(photo)
+    print("Labels detected: " + str(label_count))
+
+
+if __name__ == "__main__":
+    main()
